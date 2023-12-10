c1dea89b9a9bf72f849c239c84670bcb8ce6e3aa
ramon-passos <ramonpassos2011@gmail.com>
2023-12-10 17:01:02 -0300

    format document

--- trabalho-3/main.c
+++ trabalho-3/main.c
@@ -2,7 +2,7 @@
 // João Pedro Assumpção Evaristo, RA: 147887
 // Ramon da Silva Passos, RA: 148011
 
-// mpicc -o main main.c 
+// mpicc -o main main.c
 // mpiexec -np 4 ./main
 
 #include <stdio.h>
@@ -19,28 +19,34 @@
 
 // MPI_Recv definition
 // int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
-void sendGridToWorkers(float **readingGrid, int numProcesses) {
-    int i, j;
-    for (i = 1; i < numProcesses; i++) {
-        for (j = 0; j < DIMENSION; j++) {
-            MPI_Send(readingGrid[j], DIMENSION, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
-        }
+void sendGridToWorkers(float **readingGrid, int numProcesses)
+{
+  int i, j;
+  for (i = 1; i < numProcesses; i++)
+  {
+    for (j = 0; j < DIMENSION; j++)
+    {
+      MPI_Send(readingGrid[j], DIMENSION, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
     }
+  }
 }
 
-void receiveGridFromMaster(float **readingGrid) {
-    int i;
-    for (i = 0; i < DIMENSION; i++) {
-        MPI_Recv(readingGrid[i], DIMENSION, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+void receiveGridFromMaster(float **readingGrid)
+{
+  int i;
+  for (i = 0; i < DIMENSION; i++)
+  {
+    MPI_Recv(readingGrid[i], DIMENSION, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
 }
 
-void calculateOffset(int processId, int numProcesses, int *ini, int *end) {
-    int ini, end, numWorkers, div;
-    numWorkers = numProcesses - 1;
-    div = ceil(DIMENSION / (double)numWorkers);
-    *ini = div * (processId -1);
-    *end = div * processId < DIMENSION ? div * processId : DIMENSION;
+void calculateOffset(int processId, int numProcesses, int *ini, int *end)
+{
+  int ini, end, numWorkers, div;
+  numWorkers = numProcesses - 1;
+  div = ceil(DIMENSION / (double)numWorkers);
+  *ini = div * (processId - 1);
+  *end = div * processId < DIMENSION ? div * processId : DIMENSION;
 }
 
 // Funcao que retorna o numero de celulas
@@ -95,54 +101,64 @@ void workerPlay(float **readingGrid, float **writingGrid, int processId, int num
   }
 }
 
-void workerProcess(int processId, int numProcesses) {
-    float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
-    float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+void workerProcess(int processId, int numProcesses)
+{
+  float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+  float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
 
-    int i;
-    for (i = 0; i < DIMENSION; i++) {
-        readingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
-    }
+  int i;
+  for (i = 0; i < DIMENSION; i++)
+  {
+    readingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+  }
 
-    for (i = 0; i < DIMENSION; i++) {
-        writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
-    }
+  for (i = 0; i < DIMENSION; i++)
+  {
+    writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+  }
 
-    receiveGridFromMaster(readingGrid);
-    workerPlay(readingGrid, writingGrid, processId, numProcesses);
+  receiveGridFromMaster(readingGrid);
+  workerPlay(readingGrid, writingGrid, processId, numProcesses);
 }
 
-void masterProcess(int numProcesses) {
-    float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
-    float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+void masterProcess(int numProcesses)
+{
+  float **readingGrid = (float **)malloc(DIMENSION * sizeof(float *));
+  float **writingGrid = (float **)malloc(DIMENSION * sizeof(float *));
 
-    int i;
-    for (i = 0; i < DIMENSION; i++) {
-        readingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
-    }
+  int i;
+  for (i = 0; i < DIMENSION; i++)
+  {
+    readingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+  }
 
-    for (i = 0; i < DIMENSION; i++) {
-        writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
-    }
+  for (i = 0; i < DIMENSION; i++)
+  {
+    writingGrid[i] = (float *)malloc(DIMENSION * sizeof(float));
+  }
 
-    fillZeros(readingGrid);
-    initializeGrid(readingGrid);
-    masterPlay(readingGrid, writingGrid);
+  fillZeros(readingGrid);
+  initializeGrid(readingGrid);
+  masterPlay(readingGrid, writingGrid);
 }
 
-int main(void) {
-    int processId, numProcesses;
+int main(void)
+{
+  int processId, numProcesses;
 
-    MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &processId);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &processId);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-    if(processId == 0) {
-        masterProcess(numProcesses);
-    } else {
-        workerProcess(processId, numProcesses);
-    }
+  if (processId == 0)
+  {
+    masterProcess(numProcesses);
+  }
+  else
+  {
+    workerProcess(processId, numProcesses);
+  }
 
-    MPI_Finalize();
-    return 0;
+  MPI_Finalize();
+  return 0;
 }
