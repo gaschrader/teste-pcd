2bb673ea19d43670ce52b5e916c3024a672298cf
joao.evaristo <joao.evaristojw@gmail.com>
2023-12-10 20:05:54 -0300

    fix: remove comments

--- trabalho-3/mpi_utils.h
+++ trabalho-3/mpi_utils.h
@@ -2,15 +2,6 @@
 #include "common.h"
 #include "mpi.h"
 
-// MPI_Send definition
-// int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
-
-// MPI_Recv definition
-// int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
-
-// MPI_REDUCE definition
-// int MPI_Reduce(const void sendbuf, voidrecvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm)
-
 void sendGridToWorkers(float **readingGrid, int numProcesses)
 {
   int i, j;
@@ -55,13 +46,15 @@ void receiveGridFromMaster(float **readingGrid)
   }
 }
 
-void getAliveCellsFromWorkers(int *sum) {
+void getAliveCellsFromWorkers(int *sum)
+{
   int aux = 0;
 
   MPI_Reduce(&aux, sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 }
 
-void sendAliveCellsToMaster(int sum) {
+void sendAliveCellsToMaster(int sum)
+{
   int aux;
 
   MPI_Reduce(&sum, &aux, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
